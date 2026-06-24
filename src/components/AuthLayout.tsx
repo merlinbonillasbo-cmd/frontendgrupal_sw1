31e8021cf097367be9a460ffeae09ad9c8eb6cf5
CodeWithElias <eliaspumaclaure48@gmail.com>
2026-06-23 22:08:11 -0400

    imagen de perfil

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import loginImg from "../../../img/login.jpeg";
+import loginImg from "../assets/perfil.avif";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
